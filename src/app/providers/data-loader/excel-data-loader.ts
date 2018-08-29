da28cb2552420c085470740e9b1f78dbf6498426
Pascal Fluck <pascal.fluck@agileforge.ch>
2018-08-29 15:15:13 +0200

    Implements reading of Excel file

--- src/app/providers/data-loader/excel-data-loader.ts
+++ src/app/providers/data-loader/excel-data-loader.ts
@@ -3,16 +3,44 @@ import { DataLoader } from './data-loader';
 import { FileService } from '../file/file.service';
 import { MergeableRowDataModel } from './mergeableRowDataModel';
 import { Observable } from 'rxjs';
+import * as excelToJson from 'convert-excel-to-json';
+import { LogService } from '../log-service';
 
 @Injectable()
 export class ExcelDataLoader extends DataLoader {
 
-    constructor(private _fileService: FileService) {
+    constructor(private _logger: LogService) {
         super();
     }
 
+    /**
+     * Load data from an Excel file and return it as key/value simple structure.
+     * @abstract
+     * @param {string} fileName The file where are the data.
+     * @returns {{ [fieldName: string]: object }} The built data.
+     * @memberof DataLoader
+     */
     load(fileName: string): Observable<MergeableRowDataModel[]> {
-        return null;
+        this._logger.debug(`Starting to load data from Excel file '${fileName}'.`);
+        return Observable.create(new Promise((resolve, reject) => {
+            try {
+                const result = excelToJson({
+                    sourceFile: fileName,
+                    columnToKey: {
+                        '*': '{{columnHeader}}'
+                    }
+                });
+
+                const sheetOneKey = Object.keys(result)[0];
+                const sheetOneData = <MergeableRowDataModel[]>result[sheetOneKey];
+
+                this._logger.info(`Data from Excel file '${fileName}' loaded successfully.`);
+                resolve(sheetOneData);
+            } catch (error) {
+                this._logger.error(`Loading from Excel file '${fileName}' fail with error:\n${error}`);
+                reject(error);
+            }
+        }));
     }
 
 }
