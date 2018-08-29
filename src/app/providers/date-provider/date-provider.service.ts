93cf7541eb7c4648abed7339791ba517ea81422e
Pascal Fluck <pascal.fluck@agileforge.ch>
2018-08-29 20:53:37 +0200

    Add date provider service for tests

---
+++ src/app/providers/date-provider/date-provider.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * Current date service provider. It seems stupid,
+ * but this allow to mock date in tests.
+ * @export
+ * @class DateProviderService
+ */
+@Injectable({
+    providedIn: 'root'
+})
+export class DateProviderService {
+
+    constructor() { }
+
+    /**
+     * Returns the current date and time. It seems stupid,
+     * but this allow to mock date in tests.
+     * @returns {Date} The current date and time.
+     * @memberof DateProviderService
+     */
+    now(): Date {
+        return new Date();
+    }
+
+}
