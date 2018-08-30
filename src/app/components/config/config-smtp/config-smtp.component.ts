90f5075e7762c6e5c351bacc8f68225804a4cba8
Pascal Fluck <pascal.fluck@agileforge.ch>
2018-08-30 12:31:19 +0200

    Set fields for SMTP

--- src/app/components/config/config-smtp/config-smtp.component.ts
+++ src/app/components/config/config-smtp/config-smtp.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ConfigService } from '../../../providers/config/config.service';
+import { SenderConfigModel, SmtpConfigModel } from '../../../providers/config/configModel';
 
 @Component({
-  selector: 'app-config-smtp',
-  templateUrl: './config-smtp.component.html',
-  styleUrls: ['./config-smtp.component.scss']
+    selector: 'app-config-smtp',
+    templateUrl: './config-smtp.component.html',
+    styleUrls: ['./config-smtp.component.scss']
 })
 export class ConfigSmtpComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    smtp: FormGroup;
+
+    constructor(
+        private _formBuilder: FormBuilder,
+        private _configService: ConfigService,
+    ) { }
+
+    ngOnInit() {
+        this._configService.config.smtp = this._configService.config.smtp || <SmtpConfigModel>{};
+        const fb = this._formBuilder;
+        const config = this._configService.config.smtp;
+
+        this.smtp = fb.group({
+            host: fb.control(config.host, [Validators.required]),
+            port: fb.control(config.port, Validators.pattern(/\d*/)),
+            isSsl: fb.control(config.isSsl),
+            userName: fb.control(config.userName, Validators.required),
+            password: fb.control(config.password, Validators.required),
+        });
+    }
+
 
 }
