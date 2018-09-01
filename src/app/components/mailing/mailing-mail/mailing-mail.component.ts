caa37b6d153caa80423ff49d21c044a70d84abdd
Pascal Fluck <pascal.fluck@agileforge.ch>
2018-09-01 21:24:29 +0200

    Implementation of email data form

--- src/app/components/mailing/mailing-mail/mailing-mail.component.ts
+++ src/app/components/mailing/mailing-mail/mailing-mail.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MailingDataModel } from '../../../providers/mailer-engine/mailingDataModel';
 
 @Component({
-  selector: 'app-mailing-mail',
-  templateUrl: './mailing-mail.component.html',
-  styleUrls: ['./mailing-mail.component.scss']
+    selector: 'app-mailing-mail',
+    templateUrl: './mailing-mail.component.html',
+    styleUrls: ['./mailing-mail.component.scss']
 })
 export class MailingMailComponent implements OnInit {
 
-  constructor() { }
+    @Input() set mailingData(value: MailingDataModel) { this.setMailingData(value); }
+    get mailingData(): MailingDataModel { return this._mailingData; }
 
-  ngOnInit() {
-  }
+    email: FormGroup;
+
+    private _mailingData: MailingDataModel;
+
+    constructor(
+        private _formBuilder: FormBuilder
+    ) { }
+
+    ngOnInit() {
+        const fb = this._formBuilder;
+        this.email = fb.group({
+            subject: fb.control(null, Validators.required),
+            body: fb.control(null, Validators.required)
+        });
+
+        this.email.valueChanges.subscribe(() => {
+            if (this.mailingData) {
+                this.mailingData.subject = this.email.get('subject').value;
+                this.mailingData.body = this.email.get('body').value;
+            }
+        });
+    }
+
+    private setMailingData(value: MailingDataModel): void {
+        this._mailingData = value;
+
+        if (value && this.email) {
+            this.email.setValue({
+                subject: value.subject,
+                body: value.body,
+            }, { emitEvent: false });
+        }
+    }
 
 }
