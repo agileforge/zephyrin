95f9eae71e79d129d0e0ff740ec0f2938e2895c0
Pascal Fluck <pascal.fluck@agileforge.ch>
2018-09-05 11:31:06 +0200

    Put in place build

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,15 @@
+import { by, element } from 'protractor';
 import { AngularElectronPage } from './app.po';
-import { browser, element, by } from 'protractor';
 
-describe('angular-electron App', () => {
-  let page: AngularElectronPage;
+describe('zephyrin App', () => {
+    let page: AngularElectronPage;
 
-  beforeEach(() => {
-    page = new AngularElectronPage();
-  });
+    beforeEach(() => {
+        page = new AngularElectronPage();
+    });
 
-  it('should display message saying App works !', () => {
-    page.navigateTo('/');
-    expect(element(by.css('app-home h1')).getText()).toMatch('App works !');
-  });
+    it('should display message saying App works !', () => {
+        page.navigateTo('/');
+        expect(element(by.css('app-home h1')).getText()).toMatch('App works !');
+    });
 });
